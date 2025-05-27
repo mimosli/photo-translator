8b0096ebbb2e185dfb947b57c45a8d9873405f2b
mimosli <weinzierl.lisa@gmx.net>
2025-05-27 15:09:25 +0000

    premier test translate success

--- ocr.py
+++ ocr.py
@@ -2,4 +2,10 @@ from PIL import Image
 import pytesseract
 
 def extract_text(image_path):
+    print(pytesseract.image_to_string(Image.open(image_path), lang='deu'))
     return pytesseract.image_to_string(Image.open(image_path), lang='deu')
+
+
+
+sample_text = extract_text("/home/ubuntu/photo-translator/static/uploads/Screenshot 2025-05-26 174727.png")
+print(sample_text)
--- translate.py
+++ translate.py
@@ -3,11 +3,14 @@ import openai
 import deepl
 from dotenv import load_dotenv
 
+import ocr
+from ocr import sample_text
+
 load_dotenv()
 
 def translate_with_deepl(text):
     translator = deepl.Translator(os.getenv("DEEPL_API_KEY"))
-    return translator.translate_text(text, source_lang="DE", target_lang="EN").text
+    return translator.translate_text(text, source_lang="DE", target_lang="EN-GB").text
 
 def translate_with_gpt(text):
     openai.api_key = os.getenv("OPENAI_API_KEY")
@@ -19,3 +22,8 @@ def translate_with_gpt(text):
         ]
     )
     return response.choices[0].message.content.strip()
+
+sample_translated_dp = translate_with_deepl(sample_text)
+#sample_translated_oai = translate_with_gpt(sample_text)
+print(sample_translated_dp)
+#print(sample_translated_oai)
